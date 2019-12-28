1385d46eac7f771e8bb817f27e7cbdaf557d0558
Anh Tran <fanomanu@gmail.com>
2019-12-28 14:28:29 -0600

    Account UI

--- app/src/main/java/com/example/hf/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/hf/ui/account/AccountFragment.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hf.R;
@@ -23,8 +20,7 @@ public class AccountFragment extends Fragment {
     dashboardViewModel =
         ViewModelProviders.of(this).get(AccountViewModel.class);
     View root = inflater.inflate(R.layout.fragment_account, container, false);
-    final TextView textView = root.findViewById(R.id.text_dashboard);
-    dashboardViewModel.getText().observe(this, textView::setText);
     return root;
   }
+
 }
--- app/src/main/java/com/example/hf/ui/account/AccountViewModel.java
+++ app/src/main/java/com/example/hf/ui/account/AccountViewModel.java
@@ -1,19 +1,10 @@
 package com.example.hf.ui.account;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class AccountViewModel extends ViewModel {
 
-  private MutableLiveData<String> mText;
-
   public AccountViewModel() {
-    mText = new MutableLiveData<>();
-    mText.setValue("This is account fragment");
   }
 
-  LiveData<String> getText() {
-    return mText;
-  }
 }
