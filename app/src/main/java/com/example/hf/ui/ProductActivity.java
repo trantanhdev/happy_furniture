039c2e1057037939eb47cba8cb99fc3858369b50
Anh Tran <fanomanu@gmail.com>
2020-02-20 13:58:24 -0600

    Merge Product data to Product activity UI

--- app/src/main/java/com/example/hf/ui/ProductActivity.java
+++ app/src/main/java/com/example/hf/ui/ProductActivity.java
@@ -1,11 +1,13 @@
 package com.example.hf.ui;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.hf.R;
 import com.example.hf.models.data.Product;
 import com.example.hf.repositories.ProductRepository;
+import com.example.hf.util.Constaint;
 
 import javax.inject.Inject;
 
@@ -29,7 +31,8 @@ public class ProductActivity extends DaggerAppCompatActivity {
       value = b.getInt("pid");
 
     Product product = repository.get(value);
-    //Toast.makeText(this, "product Id:" + product.getName(), Toast.LENGTH_LONG).show();
-
+    ((ImageView) findViewById(R.id.iv_profile)).setImageResource(product.getProfileImage());
+    ((TextView) findViewById(R.id.tv_name)).setText(product.getName());
+    ((TextView) findViewById(R.id.tv_price)).setText(Constaint.CURRENCY + product.getPrice());
   }
 }
