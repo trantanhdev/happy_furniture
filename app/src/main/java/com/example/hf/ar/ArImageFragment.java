1ad6e640697b646b9b53b095fc46cfcb3c5a561b
Anh Tran <fanomanu@gmail.com>
2020-03-02 17:32:24 +0700

    Add Augmented Image

---
+++ app/src/main/java/com/example/hf/ar/ArImageFragment.java
@@ -0,0 +1,72 @@
+package com.example.hf.ar;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import com.example.hf.R;
+import com.example.hf.models.ArSourceType;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.AugmentedImageDatabase;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Session;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+
+import java.util.Collection;
+
+public class ArImageFragment extends ArFragment {
+
+  private static final String TAG = ArImageFragment.class.getSimpleName();
+
+  @Override
+  protected Config getSessionConfiguration(Session session) {
+    Config config = super.getSessionConfiguration(session);
+    config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+    config.setFocusMode(Config.FocusMode.AUTO);
+    loadImageDatabase(config, session);
+    return config;
+  }
+
+  private void loadImageDatabase(Config config, Session session) {
+    Bitmap bonsaiBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bonsai);
+    AugmentedImageDatabase aid = new AugmentedImageDatabase(session);
+    aid.addImage("bonsai", bonsaiBitmap);
+    config.setAugmentedImageDatabase(aid);
+  }
+
+  @Override
+  public void onUpdate(FrameTime frameTime) {
+    Frame frame = getArSceneView().getArFrame();
+    Collection<AugmentedImage> images = frame.getUpdatedTrackables(AugmentedImage.class);
+
+    for (AugmentedImage image : images) {
+      if (image.getTrackingState().equals(TrackingState.TRACKING)) {
+        if ("bonsai".equals(image.getName())) {
+          Anchor anchor = image.createAnchor(image.getCenterPose());
+          ArSourceBuilder.buildModel(getContext(), ArSourceType.DEVICE, image.getName())
+              .thenAccept(builtModel -> placeModel(anchor, builtModel))
+              .exceptionally(
+                  throwable -> {
+                    Log.e(TAG, "Unable to load Renderable.", throwable);
+                    return null;
+                  });
+        }
+      }
+    }
+
+    super.onUpdate(frameTime);
+  }
+
+  public void placeModel(Anchor anchor, ModelRenderable modelRenderable) {
+    AnchorNode anchorNode = new AnchorNode(anchor);
+    anchorNode.setRenderable(modelRenderable);
+    getArSceneView().getScene().addChild(anchorNode);
+  }
+
+}
